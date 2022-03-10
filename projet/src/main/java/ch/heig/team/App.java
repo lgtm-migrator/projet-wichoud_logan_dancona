1b8f2f6cba18edabfbd85d0e6a1bf2a9e2d1645e
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-03-10 18:10:43 +0100

    created 4 commands

--- projet/src/main/java/ch/heig/team/App.java
+++ projet/src/main/java/ch/heig/team/App.java
@@ -2,23 +2,32 @@ package ch.heig.team;
 
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
+import picocli.CommandLine.*;
 
 @Command(
         name = "name",
         description = "",
         mixinStandardHelpOptions = true,
         version = "0.1",
-        subcommands = {}
+        subcommands = {
+                newCommand.class,
+                cleanCommand.class,
+                buildCommand.class,
+                serveCommand.class
+        }
 )
 
 public class App implements Runnable {
+
+    @Parameters(index = "0", description = "the mode you want to choose. ")
+    @Option(names = {"-n", "--new"}, description = "new")
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
     }
 
     @Override
     public void run() {
+        CommandLine.usage(this, System.out);
         System.out.println("hello");
     }
 }
